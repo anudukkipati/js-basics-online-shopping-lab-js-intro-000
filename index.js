b0ff4ca57dcc5e8e6b6c6d94025be77add60b188
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-26 03:29:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function placeOrder(cardNumber) {
   if(Number.isInteger(cardNumber)) {
     delete cart;
     return`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  } else {
+  } //else {
     return `Sorry, we don't have a credit card on file for you.`
-  }
+//  }
 }
