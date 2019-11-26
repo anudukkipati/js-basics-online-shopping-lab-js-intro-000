353893b47497861028d744f4a5ea36c2d6aea62e
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-26 04:03:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if(Number.isInteger(cardNumber)) {
 
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   } //else {
     return `Sorry, we don't have a credit card on file for you.`
