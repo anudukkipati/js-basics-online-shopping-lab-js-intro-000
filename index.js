9b93725779111bf3b5c53fe0fda4fcc420cf95a6
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-26 03:00:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
     if(item === cart[i]["itemName"]) {
-      cart.splice(cart.indexOf(cart[i]), 1);
+      return cart.splice(cart.indexOf(cart[i]), 1);
     } else  {
       return `That item is not in your cart.`;
     }
